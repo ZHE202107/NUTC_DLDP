fd218bcc87f09da4041168730104a13338606f57
7ooae979@gmail.com <7ooae979@gmail.com>
2022-03-30 14:51:44 +0800

    新增 blocking (尚未運行)

--- homework2/Blocking /blocking.v
+++ homework2/Blocking /blocking.v
@@ -1,34 +1,33 @@
 module blocking (
-  clk,
-  rst_n,
-  a_i,
-  b_i,
-  a_o,
-  b_o
-  );
+    clk,
+    rst_n,
+    a_i,
+    b_i,
+    a_o,
+    b_o
+    );
 
-  input clk;
-  input rst_n;
-  input a_i, b_i:
-  output a_o,b_o;
+    input clk;
+    input rst_n;
+    input a_i, b_i:
+    output a_o,b_o;
+    
+    reg a, b;
   
-  reg a, b;
+    assign a_o = a;
+    assign b_o = b;
   
-  assign a_o = a;
-  assign b_o = b;
+    assign a_o = a;
+    assign b_o = b;
   
-  assign a_o = a;
-  assign b_o = b;
-  
-  always @(posedge clk or negedge rst in) begin
-    if (~rst _n) begin
-      a = a_i; 
-      b = b_i;
-    end 
-    else begin
-      a = b;
-      b = a;
+    always @(posedge clk or negedge rst in) begin
+        if (~rst _n) begin
+            a = a_i; 
+            b = b_i;
+        end 
+        else begin
+            a = b;
+            b = a;
+        end
     end
-  end
-  
-  endmodule
+endmodule

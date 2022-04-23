c7f4afb538627d151863f016a173182d3417cecb
ZHE202107 <7ooae979@gmail.com>
2022-04-24 05:37:28 +0800

    從if改寫成case敘述

--- homework3/voter_case.v
+++ homework3/voter_case.v
@@ -3,27 +3,34 @@ module voter_case(I, O);
     output  [3:1] O;  // O Result
     reg     [3:1] O;
 
-    always@(I)
-    begin
+    always@(I) begin
         // 反對
-        if (I == 4'b0000) O[3] = 1;
-        else if (I == 4'b0001) O[3] = 1;
-        else if (I == 4'b0010) O[3] = 1;
-        else if (I == 4'b0100) O[3] = 1;
-        else if (I == 4'b1000) O[3] = 1;
-        else O[3] = 0;
+        case(I)
+            4'b0000: O[3] = 1;
+            4'b0001: O[3] = 1;
+            4'b0010: O[3] = 1;
+            4'b0100: O[3] = 1;
+            4'b1000: O[3] = 1;
+            default: O[3] = 0;
+        endcase
         // 平手
-        if (I == 4'b0011) O[2] = 1;         
-        else if (I == 4'b0101) O[2] = 1;
-        else if (I == 4'b0110) O[2] = 1;
-        else if (I == 4'b1001) O[2] = 1;
-        else if (I == 4'b1010) O[2] = 1;
-        else if (I == 4'b1100) O[2] = 1;
-        else O[2] = 0;
+        case(I)
+            4'b0011: O[2] = 1;
+            4'b0101: O[2] = 1;
+            4'b0110: O[2] = 1;
+            4'b1001: O[2] = 1;
+            4'b1010: O[2] = 1;
+            4'b1100: O[2] = 1;
+            default: O[2] = 0;
+        endcase
         // 贊成
-        if (I == 4'b1110 || I == 4'b1101 || I == 4'b1011 || I == 4'b0111 || I == 4'b1111)
-            O[1] = 1;
-        else
-            O[1] = 0;
+        case(I)
+            4'b0111: O[1] = 1;
+            4'b1011: O[1] = 1;
+            4'b1101: O[1] = 1;
+            4'b1110: O[1] = 1;
+            4'b1111: O[1] = 1;
+            default: O[1] = 0;
+        endcase
     end
 endmodule
